d9307d2bdce035a58ef7dabfafa8057122a506a5
jasvinderkhera <ankitkhera15@gmail.com>
2024-12-12 11:33:42 +0530

    plans page corrected

--- src/components/plans/plans.jsx
+++ src/components/plans/plans.jsx
@@ -6,14 +6,17 @@ function Plans() {
     <div>
       <h3 className='fw-bold'>Cloud Storage Plan</h3>
       <p className="text-center fs-5">Secure and Reliable cloud storage for Your Data </p>
-      <div className="row gap-3">
-        <div className="col md-2">
-          
+      <div className="row gap-3 flex-column flex-md-row">
+        
+        <div className="col md-6 freePlan rounded-5 text-white">
+        <h4 className="text-dark bg-white px-4 py-2 rounded-5 text-center">Free Version</h4>
+        <p className=' fs-5 text-center'>Free</p>
+        <p className=" text-center mb-2">1MB speed</p>
+        <p className=" text-center mb-2">photo</p>
+        <p className=" text-center mb-2">Low Quality</p>
+        <p className=" text-center mb-2">500MB Cloud Storage</p>
         </div>
-        <div className="col md-4 freePlan rounded-5">
-        {/* <h6 className="text-secondary">Basic Version</h6> */}
-        </div>
-        <div className="col md-4 paidPlan rounded-5">
+        <div className="col md-6 paidPlan rounded-5">
         <h4 className="text-dark bg-white px-4 py-2 rounded-5 text-center">Paid Version</h4>
         <p className=' fs-5 text-center'>₹ 199/Month</p>
         <p className=" text-center mb-2">10MB speed</p>
@@ -21,7 +24,6 @@ function Plans() {
         <p className=" text-center mb-2">High Quality</p>
         <p className=" text-center mb-2">5GB Cloud Storage</p>
         </div>
-        <div className="col md-2"></div>
       </div>
     </div>
   )
